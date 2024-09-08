5fe365416a415d9712b7b6a8ccf82299e4b2685e
pranav <pranav8267@gmail.com>
2024-09-09 00:16:45 +0530

    add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { Button, useColorMode } from "@chakra-ui/react";
+import Icon from '../../public/icon.png'
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   
   return (
-    <div className="py-3 px-6 sticky h-20 top-0 left-0 w-full z-30 bg-gray-800 flex justify-between items-center backdrop-blur-md shadow transition-all duration-500 ease-in-out">
+    <div className="py-3 px-6 sticky h-20 top-0 left-0 w-full z-30 bg-gray-800 flex items-center backdrop-blur-md shadow transition-all duration-500 ease-in-out gap-3 text-4xl">
     {/* <div className=" p-4 sticky top-0 left-0 bg-gray-800 text-white rounded-lg shadow-lg transition-all duration-500 ease-in-out items-center justify-between"> */}
+      <img src={Icon} alt="ICON" />
        <h1 className="text-3xl font-semibold text-center text-[#81e6d9]">Postpal</h1>
-      {/* <h3>📪</h3> */}
       {/* <Button onClick={toggleColorMode}>
         {colorMode === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}
       </Button> */}
